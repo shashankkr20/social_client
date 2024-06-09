26de1689a7cf5b9cb9bd324b8635aa7e46877c6f
pandiji <skm808478@gmail.com>
2024-06-09 16:34:58 +0530

    njbj

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // let BASE_URL = "https://post-it-heroku.herokuapp.com/";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  let BASE_URL = "https://social-backend-krwj.onrender.com";
+  let BASE_URL = "https://social-backend-krwj.onrender.com/";
 
 
 export { BASE_URL };
